94de06f20ea61407cf1c7ddd1eb2feafb2122f4a
MSuiram <marius.valard@gmail.com>
2025-02-27 10:33:29 +0100

    Add Fonction of Utils

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import math
+from scipy.integrate import quad
+
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -5,7 +8,7 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	return math.factorial(n)
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -14,7 +17,13 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	n = b**2-4*a*c
+	if n > 0:
+		return(((-b+math.sqrt(n))/(2*a),(-b-math.sqrt(n))/(2*a)))
+	if n == 0:
+		return((-b/(2*a)))
+	if n < 0:
+		return()
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -29,7 +38,15 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	if lower > upper:
+		return None
+	else:
+		def fonct(x):
+			return eval(function, {'x': x})
+
+		res, error = quad(fonct, lower, upper)
+
+		return res
 
 if __name__ == '__main__':
 	print(fact(5))
